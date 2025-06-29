3e3e79a75ca2662e59477a6cabcc3f4b655e6d00
Chloe-R-Rowley <chloeruthrowley2002@gmail.com>
2025-06-29 09:39:24 +0530

    theme data and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'User App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF6750A4),
+          onPrimary: Color(0xFFFFFFFF),
+          primaryContainer: Color(0xFFEADDFF),
+          onPrimaryContainer: Color(0xFF21005D),
+          secondary: Color(0xFF625B71),
+          onSecondary: Color(0xFFFFFFFF),
+          secondaryContainer: Color(0xFFE8DEF8),
+          onSecondaryContainer: Color(0xFF1D192B),
+          tertiary: Color(0xFF7D5260),
+          onTertiary: Color(0xFFFFFFFF),
+          tertiaryContainer: Color(0xFFFFD8E4),
+          onTertiaryContainer: Color(0xFF31111D),
+          error: Color(0xFFBA1A1A),
+          onError: Color(0xFFFFFFFF),
+          errorContainer: Color(0xFFFFDAD6),
+          onErrorContainer: Color(0xFF410002),
+          surface: Color(0xFFFFFBFE),
+          onSurface: Color(0xFF1C1B1F),
+        ),
       ),
       home: Scaffold(body: const Center(child: Text('Hello World'))),
     );
